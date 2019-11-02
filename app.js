9e234040f5e1f34b2c33db25ee631456fbf6ceb6
obusorezekiel <obusorezekiel@gmail.com>
2019-11-02 18:57:15 +0100

    Made major changes to the UI

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 
 const app = express();
 
@@ -8,11 +9,15 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')))
 
 
-app.use(adminRoutes)
+app.use('/admin', adminRoutes)
 app.use(shopRoutes)
 
+app.use((req,res,next) => {
+    res.sendFile(path.join(__dirname,'views', '404.html'))
+});
 
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const path = require('path')
 
-router.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+const rootDir = require('../util/path')
+
+router.get('/add-product', (req, res, next) => {
+    res.sendFile(path.join(rootDir ,'views', 'add-product.html'))
   });
 
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
     console.log(req.body)
     res.redirect('/')
 })
