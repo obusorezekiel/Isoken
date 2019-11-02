4ada6f706621417db34daf3f1a538c64286d2e22
obusorezekiel <obusorezekiel@gmail.com>
2019-11-02 20:00:57 +0100

    Introduced PUG and modified the UI

--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,11 @@ const path = require('path')
 const rootDir = require('../util/path')
 const router = express.Router()
 
+const adminData = require('./admin')
+
 router.get('/', (req,res,next) => {
-    res.sendFile(path.join(rootDir ,'views', 'shop.html'))
+    const products = adminData.products
+    res.render('shop', {prods: products, docTitle: 'Shop'})
 })
 
 module.exports = router;
