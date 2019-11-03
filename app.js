97ab5550945da6c1af5d033e850f17deda5071ff
obusorezekiel <obusorezekiel@gmail.com>
2019-11-03 11:51:41 +0100

    Created Controllers and Models

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const errorController= require('./controllers/error')
 
 const app = express();
 
@@ -18,8 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404)
 
 app.listen(3800);
