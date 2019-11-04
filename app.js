2be6e5d2d2b3b36fc6df5a8423f601d287135a03
obusorezekiel <obusorezekiel@gmail.com>
2019-11-04 15:31:42 +0100

    Connecting to MySQL

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -12,6 +11,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
