3504402fd17c8c179c72cdc749282673d9b17cca
obusorezekiel <obusorezekiel@gmail.com>
2019-11-04 02:28:36 +0100

    Created the Edit routes, cart pages, cart routes and cart models

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product')
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
@@ -8,6 +9,13 @@ exports.getProducts = (req, res, next) => {
             path: '/products',
             
           });
+    });
+}
+
+exports.getProduct = (req,res,next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        res.render('shop/product-detail', {product: product, pageTitle: product.title, path: '/products' })
     });
 }
 
@@ -25,6 +33,22 @@ exports.getCart = (req,res,next) => {
     res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart'
+    })
+}
+
+exports.postCart = (req,res,next) => {
+    const prodId = req.body.productId;
+    Product.findById(prodId, (product) => {
+        Cart.addProduct(prodId, product.price);
+    });
+    res.redirect('/cart');
+}
+
+
+exports.getOrders = (req,res,next) => {
+    res.render('shop/orders', {
+        path: '/orders',
+        pageTitle: 'Your Orders'
     })
 }
 
